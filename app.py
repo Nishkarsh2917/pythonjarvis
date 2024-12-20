3d9982c8ada36ba124d3ec1c77c9eb76d248cd0d
Nishkarsh2917 <nishkarsh743@gmail.com>
2024-12-20 09:04:14 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+def numpy_spiral_order(matrix):
+    result = []
+    while matrix.size > 0:
+        result.extend(matrix[0].tolist())
+        matrix = matrix[1:]
+
+        if matrix.size > 0:
+            matrix = np.rot90(matrix)
+    return result
+
+array = np.random.randint(1, 101, size=(5, 5))
+print("array ")
+print(array)
+
+middle_element = array[2, 2]
+print("Middle Element ", middle_element)
+
+
+row_mean = np.mean(array, axis=1)
+print("Row Means ", row_mean)
+
+mean = np.mean(array)
+new_array = array[array > mean]
+print("Elements greater than mean ", new_array)
+
+spiralorder = numpy_spiral_order(array)
+print("Elements in spiral order", spiralorder)
